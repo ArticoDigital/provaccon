02c6572d88e0ee0a1815771c5708ce5c28ea0247
juan2ramos <juan2ramos@gmail.com>
2017-04-10 10:09:43 -0500

    front

--- wp-content/themes/provaccon/assets/js/main.js
+++ wp-content/themes/provaccon/assets/js/main.js
@@ -6,3 +6,16 @@ $("#owl-carousel").owlCarousel({
     singleItem: true,
     navigationText: ["", ""],
 });
+$("#ClientsOwl").owlCarousel({
+    navigationText: ["<", ">"],
+    navigation: true,
+    slideSpeed: 400,
+    paginationSpeed: 400,
+    autoPlay : 5000,
+    items : 3, //10 items above 1000px browser width
+    itemsDesktop : [1000,5], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,3], // betweem 900px and 601px
+    itemsTablet: [600,2], //2 items between 600 and 0;
+    itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+
+});
