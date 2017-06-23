ad23f6369c27979befb3da719dcc7cb477bfa968
Juan2Ramos <juan2ramos@gmail.com>
2017-06-23 11:13:20 -0500

    pdf

--- wp-content/themes/provaccon/functions/PostPdf.php
+++ wp-content/themes/provaccon/functions/PostPdf.php
@@ -45,7 +45,7 @@ class PostPdf
     }
     private function generateHtml(){
         $html = "";
-        $html .=  "<img width='700' src='/Users/juan2ramos/Sites/provaccon/wp-content/themes/provaccon/assets/img/header.png' >";
+        $html .=  "<img width='700px' src='/wp-content/themes/provaccon/assets/img/header.png' >";
         $html .="<h1> Certificado de anclaje para soportar caidas </h1>";
         $html .= $this->metaKey['post_title']. ' - ' . $this->metaKey['post_content'];
         return $html    ;
